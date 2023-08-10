check that weighted average is computed correct

experiment/main.py
@@ -1,3 +1,4 @@
+from re import M
 import sqlite3
 import time
 import numpy as np
@@ -92,6 +93,7 @@ class Experiment:
             else:
                 self._loss_function = torch.nn.CrossEntropyLoss(weight=normalize_weights)
         else:
+            # normalize weight can be set to not a list in the dataloader to avoid the other loss
             if self.n_outputs == 2:
                 self._loss_function = torch.nn.BCEWithLogitsLoss()
             else:
@@ -104,6 +106,7 @@ class Experiment:
             if normalize_weights is not None:
                 self.train_metrics["acc_weighted"] = MulticlassAccuracy(self.n_outputs, average="weighted").to(self.device)
                 self.valid_metrics["acc_weighted"] = MulticlassAccuracy(self.n_outputs, average="weighted").to(self.device)
+                self.valid_metrics["acc_none"] = MulticlassAccuracy(self.n_outputs, average=None).to(self.device)
 
             self.train_metrics = MetricCollection(self.train_metrics)
             self.valid_metrics = MetricCollection(self.valid_metrics)
@@ -191,7 +194,7 @@ class Experiment:
             # compute loss and accuracy
             n_samples += x.shape[0]
             loss = self._loss_function(y, t)
-            metrics = self.train_metrics(y.detach(), t.detach())    
+            metrics = self.train_metrics(y.detach(), t.detach()) 
             train_loss_epoch += loss.item() * x.shape[0]
             self.logger.log({"train": {"loss": loss} | metrics}, step=self.global_step, epoch=self._epoch)
             if self._verbose > 2:
@@ -273,7 +276,26 @@ class Experiment:
             del t_test
         
         # log
+        tp, fp, tn, fn = self.valid_metrics["acc_none"]._final_state()
+        # micro average
+        tp = tp.sum(dim=0)
+        fn = fn.sum(dim=0)
+        micro = _safe_divide(tp, tp + fn)
+        print(f"micro: {micro}")
+
+        # weighted average
+        tp, fp, tn, fn = self.valid_metrics["acc_none"]._final_state()
+        score = _safe_divide(tp, tp + fn)
+        weights = tp + fn
+        print(f"weights: {weights}, weights.sum(-1, keepdim=True) {weights.sum(-1, keepdim=True)}")
+        weighted = _safe_divide(weights * score, weights.sum(-1, keepdim=True)).sum(-1)
+        print(f"weighted: {weighted}")
+
         metrics = self.valid_metrics.compute()
+        acc_none = metrics["acc_none"]
+        #print(f"acc_none: {acc_none}")
+        metrics = {k: v for k, v in metrics.items() if k != "acc_none"}
+
         self.valid_metrics.reset()
         loss = float(cumulative_loss / n_samples)        
         endtime = datetime.datetime.now().timestamp()
@@ -350,6 +372,16 @@ def run_experiment_from_config(cfg: DictConfig) -> None:
     utils.allowed_usage_time(cfg.other.gpu_time_limit)
     exp = Experiment(cfg)
     exp.iteration_over_epochs()
+
+def _safe_divide(num, denom):
+    """Safe division, by preventing division by zero.
+
+    Additionally casts to float if input is not already to secure backwards compatibility.
+    """
+    denom[denom == 0.0] = 1
+    num = num if num.is_floating_point() else num.float()
+    denom = denom if denom.is_floating_point() else denom.float()
+    return num / denom
 
 
 if __name__ == "__main__":

networks/eq_nasnet/eq_nasnet.py
@@ -86,7 +86,12 @@ class EquivariantNASNet(nn.Module):
         model_description = {}
         for i, block_args in enumerate(blocks_args):
             model_description[f"l{i}"] = block_args._asdict()
-        wandb.config.update({"model_description": model_description})
+        
+        try:
+            wandb.config.update({"model_description": model_description})
+        except:
+            # if wandb is not initialized during NAS
+            pass
         
         # The channel sizes is first an increase factor. After that it is the number of channels
         self.blocks_args = get_channel_sizes(stem_channels, blocks_args, width_coefficient)