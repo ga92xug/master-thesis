61b24bc25296962fec6a46127b58c8f9815f7b71
ga92xug <stefan.m.frisch@gmail.com>
2023-08-15 17:16:47 +0200

    run_strategies.py

--- NAS/common_best_architecture/run_strategies.py
+++ NAS/common_best_architecture/run_strategies.py
@@ -1,3 +1,4 @@
+import re
 from turtle import st
 import pandas as pd
 import sys
@@ -8,64 +9,13 @@ sys.path.append(f"{os.getcwd()}")
 from NAS.util import encode_parameters
 from experiment.run_files.run_command import run_command
 
-choice_2_range_params = {
-    "group": [1, 2, 4, 8, 16],
-}
-
-strategies_df = pd.read_csv('NAS/data/common_best_architecture/pure_strategies.csv', index_col=0)
-
-
-global_args = ["model=eq_nasnet"]
-for strategy_row in strategies_df.iterrows():
-    strategy_name = "_".join(strategy_row[0].split('_')[1:])
-    print(f"\nPure strategy: {strategy_name}")
-    for dataset_row in strategies_df.iterrows():
-        dataset_name = "_".join(dataset_row[0].split('_')[1:])
-        print(f"Dataset: {dataset_name}")
-        
-        strategy_dict = strategy_row[1].to_dict()
-        dataset_dict = dataset_row[1].to_dict()
-
-        # we replace the (X_reflection, X_group) of strategy with the one of dataset
-        for key in strategy_dict.keys():
-            if '_reflection' in key or '_group' in key:
-                strategy_dict[key] = dataset_dict[key]
-
-        blocks_args = encode_parameters(strategy_dict, choice_2_range_params)
-
-        # Create a new run
-        args = [
-            f"training={dataset_name}-training",
-            f"wandb.tags=[eqnasnet,find_best,pure_strategy]",
-            f"model.blocks_args={blocks_args}",
-            f"model.dropout_rate={strategy_dict['-1_dropout_rate']}",
-            f"model.eq_expand_ratio={strategy_dict['-1_expand_ratio']}",
-            f"wandb.give_name=pure_strategy_{strategy_name}"
-        ]
-
-        # if strategy_name == "galaxy10" and dataset_name == "cifar10":
-        #     run_command(args, global_args, test=False, path="experiment/")
-        # elif strategy_name == "mnist_rot" and dataset_name == "galaxy10":
-        #     args.append("training.dataset.batch_size=64")
-        #     args.append("training.dataset.eval_batch_size=64")
-        #     args.append("training.accumulate=4")
-        #     run_command(args, global_args, test=False, path="experiment/")
-
-        if dataset_name == "cifar10":
-            args.append("training.dataset.rotation=True")
-            args.append("training.dataset.name=cifar10_rot")
-            run_command(args, global_args, test=False, path="experiment/")
-
-
-
-
-
-
 
 def get_adjusted_dict(
         dataset_name: str, 
         strategies_df: pd.DataFrame,
+        strategy_name: str,
         strategy_dict: dict,
+        adjust_group: bool,
     ):
     map_strategies_2_replacement_groups = {
         "cifar10": "cifar10",
@@ -75,15 +25,92 @@ def get_adjusted_dict(
         "mnist_rot": "mnist_rot",
         "ISIC_2019": "galaxy10"
     }
-    replacement_group_strategy = "strategy_" + map_strategies_2_replacement_groups[dataset_name]
+    replacement_group = map_strategies_2_replacement_groups[dataset_name]
+    replacement_group_strategy = "strategy_" + replacement_group
 
-    replacement_group_strategy_row = strategies_df.loc[[replacement_group_strategy]]
-    replacement_group_strategy_dict = replacement_group_strategy_row.to_dict()
+    if replacement_group != strategy_name and adjust_group:
+        adjusted = "_group_adjusted"
 
-    # we replace the (X_reflection, X_group) of strategy with the one of dataset
-    for key in strategy_dict.keys():
-        if '_reflection' in key or '_group' in key:
-            strategy_dict[key] = replacement_group_strategy_dict[key]
+        replacement_group_strategy_row = strategies_df.loc[[replacement_group_strategy]]
+        replacement_group_strategy_dict = replacement_group_strategy_row.squeeze().to_dict()
+
+        # we replace the (X_reflection, X_group) of strategy with the one of dataset
+        for key in strategy_dict.keys():
+            if '_reflection' in key or '_group' in key:
+                strategy_dict[key] = replacement_group_strategy_dict[key]
+
+    else:
+        adjusted = ""
+        replacement_group_strategy = ""
+
+    return strategy_dict, replacement_group_strategy, adjusted
+
+def get_training_args(
+        dataset: str,
+        strategy_name: str,
+        adjusted: str,
+        replacement_group_strategy: str,
+        strategy_dict: dict,
+        blocks_args: str,
+    ):
+    # Create a new run
+    args = [
+        f"wandb.tags=[eqnasnet,find_best,pure_strategy]",
+        f"model.blocks_args={blocks_args}",
+        f"model.dropout_rate={strategy_dict['-1_dropout_rate']}",
+        f"model.eq_expand_ratio={strategy_dict['-1_expand_ratio']}",
+        f"+model.replacement_group_strategy={replacement_group_strategy}",
+        f"wandb.give_name=pure_strategy_{strategy_name}{adjusted}"
+    ]
+
+    if dataset == "cifar10_rot":
+        training_name = "cifar10"
+        args.append("training.dataset.name=cifar10_rot")
+    elif dataset == "mnist":
+        training_name = "mnist_rot"
+        args.append("training.dataset.name=mnist")
+    else:
+        training_name = dataset
+
+    args.append(f"training={training_name}-training")
+
+    return args
+
+def start_run(
+        args: list,
+        global_args: list,
+        dataset: str,
+        max_retries: int = 4,
+    ):
+    retry_count = 0
+
+    batch_size = 128
+    accumulate = 2
+
+    while retry_count < max_retries:
+        try:
+            run_command(args, global_args, test=False, path="experiment/")
+            break
+            #batch_size = get_batch_size_from_args(args)  # Replace with the actual way to get batch size from args
+            
+        except Exception as e:
+            print(f"Exception: {e}")
+            if dataset == "galaxy10":
+                batch_size = batch_size // 2
+                accumulate = accumulate * 2
+                
+                args.append(f"training.dataset.batch_size={batch_size}")
+                args.append(f"training.dataset.eval_batch_size={batch_size}")
+                args.append(f"training.accumulate={accumulate}")
+            else:
+                # We don't retry for other datasets
+                break
+        
+        retry_count += 1
+
+    if retry_count == max_retries:
+        print("Maximum retries reached. Giving up.")
+
 
 def strategies_on_datasets(
        datasets: list,
@@ -97,38 +124,74 @@ def strategies_on_datasets(
         print(f"\nDataset: {dataset}")
         for strategy_row in strategies_df.iterrows():
             strategy_name = "_".join(strategy_row[0].split('_')[1:])
-
+            
+            if runs_to_skip(dataset, strategy_name, adjust_group):
+                continue
+            
             strategy_dict = strategy_row[1].to_dict()
-
-            if adjust_group and strategy_name != dataset:
-                get_adjusted_dict(dataset, strategies_df, strategy_dict)
+            strategy_dict, replacement_group_strategy, adjusted = get_adjusted_dict(dataset, strategies_df, strategy_name, strategy_dict, adjust_group)
+            if replacement_group_strategy != "":
+                tmp_string = f" replaced: {replacement_group_strategy}"
+            else:
+                tmp_string = ""
+            print(f"Strategy: {strategy_name}{adjusted}{tmp_string}")
+            
 
             blocks_args = encode_parameters(strategy_dict, choice_2_range_params)
 
-            # Create a new run
-            args = [
-                f"training={dataset_name}-training",
-                f"wandb.tags=[eqnasnet,find_best,pure_strategy]",
-                f"model.blocks_args={blocks_args}",
-                f"model.dropout_rate={strategy_dict['-1_dropout_rate']}",
-                f"model.eq_expand_ratio={strategy_dict['-1_expand_ratio']}",
-                f"wandb.give_name=pure_strategy_{strategy_name}"
-            ]
+            args = get_training_args(
+                dataset,
+                strategy_name,
+                adjusted,
+                replacement_group_strategy,
+                strategy_dict,
+                blocks_args,
+            )
+            
+            #print(f"Strategy dict: {strategy_dict}")
+            #print(f"Args: {args}")
 
-            run_command(args, global_args, test=False, path="experiment/")
+            start_run(args, global_args, dataset)
+
+def runs_to_skip(
+        dataset: str,
+        strategy_name: str,
+        adjust_group: bool,
+    ):
+    ############################################################################
+    # Always skip these 
+    if dataset == strategy_name and not adjust_group:
+        print(f"Skipping Strategy: {strategy_name} on {dataset}, since same with no group adjustment")
+        return True
+    
+    ############################################################################
+    # Skip these temporarily
+    if (dataset == "cifar10_rot" and strategy_name == "mnist_rot" and adjust_group) or \
+        (dataset == "cifar10_rot" and strategy_name == "mnist_rot" and not adjust_group) or \
+        (dataset == "mnist" and strategy_name == "cifar10" and not adjust_group):
+        print(f"Skipping Strategy: {strategy_name} on {dataset}, since already ran")
+        return True
+    
+    return False
+
 
 
 def main():
-    datasets = ["cifar10", "cifar10_rot", "galaxy10", "mnist", "mnist_rot"] #  "ISIC_2019"
+    # datasets = ["cifar10", "cifar10_rot", "galaxy10", "mnist", "mnist_rot"] #  "ISIC_2019"
     strategies_df = pd.read_csv('NAS/data/common_best_architecture/pure_strategies.csv', index_col=0)
-    adjust_group = True
     choice_2_range_params = {
         "group": [1, 2, 4, 8, 16],
     }
 
-    strategies_on_datasets(datasets, strategies_df, adjust_group, choice_2_range_params)
+    for adjust_group in [True, False]:
+        if adjust_group:
+            datasets = ["cifar10_rot", "mnist"]
+        else:
+            datasets = ["cifar10", "cifar10_rot", "mnist", "mnist_rot", "galaxy10"]
+
+        strategies_on_datasets(datasets, strategies_df, adjust_group, choice_2_range_params)
 
 
 
 if __name__ == "__main__":
-    
+    main()
