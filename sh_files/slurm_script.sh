da23d154c78e3cb93659e11f097baea5970f597e
ga92xug <stefan.m.frisch@gmail.com>
2023-10-06 22:55:20 +0200

    slurm_script last

--- sh_files/slurm_script.sh
+++ sh_files/slurm_script.sh
@@ -10,5 +10,8 @@
 #SBATCH -e /dss/dsshome1/lxc0A/ga96sac2/stefan/logs/run.err
 source /dss/dsshome1/lxc0A/ga96sac2/envs/isometry/bin/activate
 echo "Virtual environment activated"
-srun torchrun --standalone --nnodes=1 --nproc_per_node=1 /dss/dsshome1/lxc0A/ga96sac2/scaling-laws/train.py \
-    wandb.project=test wandb.run=test setup=test
+
+$Dataset_loc = "training.dataset.data_dir=../datasets/"
+
+srun torchrun --standalone --nnodes=1 --nproc_per_node=1 /dss/dsshome1/lxc0A/ga96sac2/stefan/scaling-laws/train.py \
+    wandb.project=SL-Scaling wandb.give_name=test_lrz $Dataset_loc
