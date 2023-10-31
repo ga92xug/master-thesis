f52afcee67b20876502d8ac992ce98dc4d6b07e6
ga92xug <stefan.m.frisch@gmail.com>
2023-10-31 21:56:07 +0100

    refactoring for exp_HPs

--- experiments/d_application_experiment/_4_adversarial_attack/run_exp.sh
+++ experiments/d_application_experiment/_4_adversarial_attack/run_exp.sh
@@ -8,16 +8,20 @@ export PY_HPO="python experiments/d_application_experiment/_3_low_data_regime/lo
 conda activate scaling-adversarial
 
 
-#$PY_SCRIPT -m other.seed=0 +exp_HPO_blood=efficientnet_pre \
-#    wandb.tags=[adversarial_attack,blood] other.should_test=True \
-#    training.adversarial_attack=Foolbox \
-#    other.verbose=2 other.debug=False other.backup_model=True \
-#    training.epochs=2
+$PY_SCRIPT -m other.seed=0 +exp_HPO_DeepDRiD=efficientnet_pre \
+    wandb.tags=[adversarial_attack,DeepDRiD] other.should_test=True \
+    training.adversarial_attack=Foolbox \
+    other.verbose=2 other.debug=False other.backup_model=True \
+    training.epochs=2
+
+# $PY_SCRIPT -m other.seed=0 '+exp_HPO_DeepDRiD=glob(*)' \
+#     wandb.tags=[adversarial_attack,DeepDRiD] other.should_test=True \
+#     training.adversarial_attack=Foolbox \
+#     other.verbose=2 other.debug=False other.backup_model=True
 
 # adversarial examples
-$PY_SCRIPT -m other.seed=0 '+exp_HPO_blood=glob(*)' \
-    wandb.tags=[adversarial_attack,blood] other.should_test=True \
-    training.adversarial_attack=Foolbox \
-    other.verbose=2 other.debug=False other.backup_model=True
+# $PY_SCRIPT -m other.seed=0 '+exp_HPO_blood=glob(*)' \
+#     wandb.tags=[adversarial_attack,blood] other.should_test=True \
+#     training.adversarial_attack=Foolbox \
+#     other.verbose=2 other.debug=False other.backup_model=True
 
-# if results are bad try other attacks
