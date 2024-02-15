b7c7fc75a108c1b6678ca93b2445f0faf42fc093
ga92xug <stefan.m.frisch@gmail.com>
2024-02-15 23:17:24 +0100

    hash state dict

--- src/utils/hash_model.py
+++ src/utils/hash_model.py
@@ -3,7 +3,7 @@ import hashlib
 
 def model_hash(model):
     # Create a SHA256 hash object
-    hash_sha256 = hashlib.sha256()
+    hash_sha256 = hashlib.sha256(usedforsecurity=False)
 
     # Iterate through model parameters and buffers
     for param in model.parameters():
@@ -16,3 +16,29 @@ def model_hash(model):
 
     # Return the hexadecimal digest of the hash
     return hash_sha256.hexdigest()
+
+def hash_state_dict(state_dict):
+    # Create a SHA256 hash object
+    hash_sha256 = hashlib.sha256(usedforsecurity=False)
+
+    # Iterate through model parameters and buffers
+    for key in state_dict.keys():
+        # Use the parameter's data converted to bytes
+        hash_sha256.update(state_dict[key].cpu().numpy().tobytes())
+
+    # Return the hexadecimal digest of the hash
+    return hash_sha256.hexdigest()
+
+def read_model_hash(file_path):
+    state_dict = torch.load(file_path)
+
+    net = state_dict["state_dict"]
+
+    for key in net.keys():
+        print(key)
+
+    print(hash_state_dict(net))
+
+
+if __name__ == "__main__":
+    read_model_hash("pre_trained_models/last.ckpt")
