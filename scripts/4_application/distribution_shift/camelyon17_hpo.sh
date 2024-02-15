dd29fcf8c2ce1d0d7f62642e1d1599885542d1ea
ga92xug <stefan.m.frisch@gmail.com>
2024-02-15 01:39:25 +0100

    camelyon

---
+++ scripts/4_application/distribution_shift/camelyon17_hpo.sh
@@ -0,0 +1,11 @@
+
+python src/main.py hparams_search=camelyon \
+    train/network=efficientnet \
+    train.network.pretrained=True \
+    
+python src/main.py hparams_search=camelyon \
+    train/network=vit \
+
+python src/main.py hparams_search=camelyon \
+    train/network=vit \
+    train.network.pretrained=True \
