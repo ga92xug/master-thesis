ff8acee759bb5cdf38bdf1734c229dfa71962e9b
ga92xug <stefan.m.frisch@gmail.com>
2024-02-21 18:23:46 +0100

    saving_loading

--- scripts/saving_loading.sh
+++ scripts/saving_loading.sh
@@ -3,12 +3,14 @@
 #    seed=0 \
 #    task_name=pretrained_test \
 #    train.trainer.max_epochs=20 \
-#    train.callbacks.model_checkpoint.save_top_k=-1
+#    train.callbacks.model_checkpoint.save_top_k=-1 \
+#    +deterministic=True \
 
 #exit 0
 # loading
 python src/main.py train=imagenette \
     train_mode=train_continue \
-    ckpt_path=/home/frischs/dev1/scaling-laws-ecnn/logs/pretrained_test/runs/2024-02-21_16-40-30/checkpoints/epoch_003.ckpt \
+    ckpt_path=/home/frischs/dev1/scaling-laws-ecnn/logs/pretrained_test/runs/2024-02-21_18-15-43/checkpoints/epoch_001.ckpt \
     task_name=pretrained_test \
-    seed=0
+    seed=0 \
+    #+deterministic=True \
