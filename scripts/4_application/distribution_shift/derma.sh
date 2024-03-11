2ce863841ef9b0f1dccc7fef63b3f6ac9b58e905
ga92xug <stefan.m.frisch@gmail.com>
2024-03-11 19:56:46 +0100

    derma

--- scripts/4_application/distribution_shift/derma.sh
+++ scripts/4_application/distribution_shift/derma.sh
@@ -1,8 +1,10 @@
 export PY_SCRIPT="python src/main.py"
+export DERMA="tags="[dist_shift_derma]" task_name=derma train=derma"
 #export SEEDS="seed=0,1,2,3,4"
 
 
-$PY_SCRIPT -m experiment=application/HPs_isic2019/efficientnet \
-    tags="[dist_shift_derma]" task_name=derma \
-    train=derma \
+#$PY_SCRIPT -m experiment=application/HPs_isic2019/efficientnet_pre \
+#    $DERMA local=encephalon
 
+$PY_SCRIPT -m experiment=application/HPs_isic2019/efficientnet \
+    $DERMA local=encephalon
