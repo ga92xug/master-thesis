d378695e7eb6b16f4f8e77fedc614bd6644dbaa9
ga92xug <stefan.m.frisch@gmail.com>
2024-03-15 20:43:44 +0100

    refactor

--- src/save_load/check_save_load.sh
+++
@@ -1,6 +0,0 @@
-
-python src/save_load/backward.py train/network/blocks_args_dict=3blocks \
-    train.dataset.resolution=4  train_mode=True seed=0
-
-python src/save_load/backward.py train/network/blocks_args_dict=3blocks \
-    train.dataset.resolution=4  train_mode=False seed=4
