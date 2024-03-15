7278a01355fcc4a3700e96430cc1b4e5f68b4808
ga92xug <stefan.m.frisch@gmail.com>
2024-03-15 16:59:35 +0100

    hps oct

--- scripts/4_application/hpo/oct.sh
+++ scripts/4_application/hpo/oct.sh
@@ -2,3 +2,12 @@
 
 python src/main.py hparams_search=ax_oct_slurm train/network=efficientnet
 python src/main.py hparams_search=ax_oct_slurm train/network=efficientnet_pre 
+
+# done 
+# tmux 1 -> vit
+# tmux 2 -> eq_nasnet
+# tmux 3 -> vit_pre
+
+# missing
+# vit_pre
+# eq_nasnet_pre
